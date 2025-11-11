99b0a97c0a99079b304aab616d5bc2562d30a346
rodreyrj <rodreyrj@gmail.com>
2025-11-11 12:55:52 -0500

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './styles.css'; // Importa los estilos de Tailwind
+import './styles.css'; // ¡Importante! Asegura que los estilos de Tailwind se carguen.
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
