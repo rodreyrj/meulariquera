8bfb1a020d34c31121631ed5c7eb2f1cda3f0b75
rodreyrj <rodreyrj@gmail.com>
2025-11-11 13:01:34 -0500

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  // Asegura que Tailwind escanee la carpeta src/ y el index.html
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      // (Mantenemos los colores y estilos personalizados)
       colors: {
         'primary-dark': '#1A1A1A',
         'secondary-dark': '#2B2B2B',
