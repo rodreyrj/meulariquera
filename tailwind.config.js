d668ac5235f021b3062e0f8764d3b6eaf2b58baa
rodreyrj <rodreyrj@gmail.com>
2025-11-11 13:31:05 -0500

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // ¡Importante! Escanea todos los archivos React
   ],
   theme: {
     extend: {
